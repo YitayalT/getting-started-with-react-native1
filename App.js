7e1d149b9737d5d152630571ca7a7bc568f614ea
YitayalT <anex518@gmail.com>
2023-07-28 10:34:29 -0800

    Adding layout

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ export default function App() {
         <Button title="Hit Me" />
       </View>
 
-      <View>
-        <Text>List of goals ... </Text>
+      <View style = {styles.goalsContainer}>
+        <Text>List of goals ...</Text>
       </View>
     </View>
   );
@@ -20,17 +20,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth:1,
+    borderBottomColor: "#ccc"
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
   },
+  goalsContainer: {
+    flex: 5
+  }
 });
